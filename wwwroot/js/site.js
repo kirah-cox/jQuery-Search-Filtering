20c05308a0f7b5e3257074545531d882cfcc67d8
unknown <kirah.cox@ENGINEERING.SNOW>
2025-10-07 09:56:01 -0600

    finished assignment

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+
+$(document).ready(function(){
+    $('#search').on('keyup', function() {
+        let value = $(this).val().toLowerCase();
+
+        $('#myTable tr').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            $(this).addClass('table-highlight');
+            if (!value) {
+                $(this).removeClass('table-highlight');
+            }
+        });
+    });
+});
